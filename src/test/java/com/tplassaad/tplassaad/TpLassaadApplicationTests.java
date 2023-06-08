532be6878a9944b282b63746ace9a2437f0324e4
AmauryGuillin <amauryg@hotmail.fr>
2023-06-08 15:58:02 +0200

    commit

---
+++ src/test/java/com/tplassaad/tplassaad/TpLassaadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tplassaad.tplassaad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpLassaadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
